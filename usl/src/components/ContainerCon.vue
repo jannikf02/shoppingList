bb021e5a4e55b2b27efb8d4c51e1b2035defbe71
eazze <il.lidan@live.de>
2022-04-26 15:36:23 +0200

    layout

---
+++ usl/src/components/ContainerCon.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer> 
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template>
+            <v-col class="mt-2" cols="12">
+              <strong>Einzukaufen:</strong>
+            </v-col>
+
+            <v-col v-for="j in 2" :key="`${n}${j}`" cols="12" md="12">
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+
+            <v-col class="mt-2" cols="12">
+              <strong>Eingekauft:</strong>
+            </v-col>
+
+            <v-col v-for="j in 2" :key="`${n}${j}`" cols="12" md="12">
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+        <v-row>
+                    <v-fab-transition>
+                        <v-btn
+                        v-show="!hidden"
+                        color="blue"
+                        dark
+                        fixed
+                        bottom
+                        right
+                        fab
+                        >
+                        <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </v-fab-transition>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "ContainerCon",
+
+  data: () => ({
+    drawer: null,
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
+};
+</script>
